3ebd1aeb4de609fe1ed78d781c5aaea7836ce41f
Yohannnn <Yohannnn@protonmail.com>
2023-08-22 15:20:02 -0700

    Add .config/nvim/lua/user/init.lua

---
+++ dot_config/nvim/lua/user/init.lua
@@ -0,0 +1,24 @@
+return {
+  colorscheme = "catppuccin",
+
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.catppuccin" },
+    { import = "astrocommunity.completion.copilot-lua" },
+    { -- further customize the options set by the community
+      "copilot.lua",
+      opts = {
+        suggestion = {
+          keymap = {
+            accept = "<C-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<C-.>",
+            prev = "<C-,>",
+            dismiss = "<C/>",
+          },
+        },
+      },
+    },
+  }
+}
